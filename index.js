125c319b489553a238d52692d0c34c9001b5e65b
fox <thefoxjones@gmail.com>
2026-01-21 12:08:59 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+const ZERO = Number.prototype.valueOf();
+const ONE = require("successor")(ZERO)
+
+const truthy = ONE;
+const falsy = ZERO;
+
 const booleanPro = {
-    getTrue: () => true, // return true
-    getFalse: () => false // return false
+    getTrue: () => Boolean(truthy), // return true
+    getFalse: () => Boolean(falsy), // return false
+    isTrue: (value) => value === Boolean(truthy), // check if value true
+    isFalse: (value) => value === Boolean(falsy), // check if value false
 };
   
 module.exports = booleanPro;
